1caa234e467259ccce8d23feef2f20b1edde85d9
General Assembly <generalassembly@tests-MacBook-Air.local>
2022-11-28 11:19:30 -0500

    commented out routes

--- server.js
+++ server.js
@@ -23,62 +23,62 @@ app.use(listController)
 // })
 
 
-//index
-app.get('/' , (req, res) => { //list will be shown on index. in show, list will show items within
-    List.find({}, (error, getList) => {
-        res.render('index.ejs', 
-        {
-            list: getList
-        })
-    })
-})
+// //index
+// app.get('/' , (req, res) => { //list will be shown on index. in show, list will show items within
+//     List.find({}, (error, getList) => {
+//         res.render('index.ejs', 
+//         {
+//             list: getList
+//         })
+//     })
+// })
 
-//new
-app.get('/new', (req, res) => {
-    res.render('new.ejs')
-})
+// //new
+// app.get('/new', (req, res) => {
+//     res.render('new.ejs')
+// })
 
-//show
-app.get('/:id/', (req, res) => {
-    List.findById(req.params.id, (error, showList) => {
-        res.render('show.ejs', 
-        {
-            list: showList
-        })
-    })
-})
+// //show
+// app.get('/:id/', (req, res) => {
+//     List.findById(req.params.id, (error, showList) => {
+//         res.render('show.ejs', 
+//         {
+//             list: showList
+//         })
+//     })
+// })
 
-//new post
-app.post('/', (req, res) => {
-    List.create(req.body, (error, List) => {
-        res.redirect('/')
-    })
-})
+// //new post
+// app.post('/', (req, res) => {
+//     List.create(req.body, (error, List) => {
+//         res.redirect('/')
+//     })
+// })
 
 
-//edit
-app.get('/:id/edit', (req, res) => {
-    List.findById(req.params.id, (error, editList) => {
-        res.render('edit.ejs', 
-        {
-            list: editList
-        })
-    })
-})
+// //edit
+// app.get('/:id/edit', (req, res) => {
+//     List.findById(req.params.id, (error, editList) => {
+//         res.render('edit.ejs', 
+//         {
+//             list: editList
+//         })
+//     })
+// })
 
-//update
-app.put('/:id/', (req, res) => {
-    List.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedList) => {
-      res.render('show.ejs', {list: updatedList})
-    })
-});
+// //update
+// app.put('/:id/', (req, res) => {
+//     List.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedList) => {
+//       res.render('show.ejs', {list: updatedList})
+//     })
+// });
 
-//delete
-app.delete('/:id', (req, res) => {
-    List.findByIdAndRemove(req.params.id, (error, data) => {
-        res.redirect('/')
-    })
-})
+// //delete
+// app.delete('/:id', (req, res) => {
+//     List.findByIdAndRemove(req.params.id, (error, data) => {
+//         res.redirect('/')
+//     })
+// })
 
 
 
