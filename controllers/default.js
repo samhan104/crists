aae22cd0bd6f18c4d42748410d72d4d87bdcce6d
General Assembly <generalassembly@tests-MacBook-Air.local>
2022-11-29 16:33:02 -0500

    id number testing

--- controllers/default.js
+++ controllers/default.js
@@ -34,7 +34,7 @@ router.post('/register' , async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         User.create([{
-            id: Date.now().toString(),
+            idNumber: Date.now().toString(),
             name: req.body.name,
             email: req.body.email,
             password: hashedPassword,
--- models/users.js
+++ models/users.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = Schema({
+  idNumber: String,
   name: {type: String, required: true},
   email: {type: String, unique: true, required: true},
   password: String,
