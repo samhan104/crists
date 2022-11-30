6df844929ad36e38d7b4ec1ee7352efa82675d3a
General Assembly <generalassembly@tests-MacBook-Air.local>
2022-11-30 00:21:37 -0500

    took out local storage save

--- controllers/default.js
+++ controllers/default.js
@@ -61,12 +61,12 @@ router.post('/login', passport.authenticate('local',{
 router.post('/register' , async (req, res) => { 
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        test.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword,
-        })
+        // test.push({
+        //     id: Date.now().toString(),
+        //     name: req.body.name,
+        //     email: req.body.email,
+        //     password: hashedPassword,
+        // })
         User.create([{
             name: req.body.name,
             email: req.body.email,
