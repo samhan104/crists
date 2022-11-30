776438c7060112d1368a900e129a894784b03384
General Assembly <generalassembly@tests-MacBook-Air.local>
2022-11-29 22:09:38 -0500

    pretty much wasted 5 hours of work, and going back to tackle login one by one

--- controllers/default.js
+++ controllers/default.js
@@ -1,29 +1,30 @@
-const express = require('express')
-const bcrypt = require('bcrypt')
-const router = express.Router()
-const seed = require('../models/seed.js')
-const User = require('../models/users.js')
-const List = require('../models/list.js')
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
 const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
-
+const User = require('../models/users.js')
+const List = require('../models/list.js')
 const initializePassport = require('../passport-config.js')
+
 initializePassport(
     passport, 
     email => users.find(user => user.email === email),
     id => users.find(user => user.id === id)
 )
 
-router.use(flash)
-router.use(
-    session({
-      secret: process.env.SECRET, 
-      resave: false, 
-      saveUninitialized: false 
-    })
-  )
+router.use(express.urlencoded({extended: false}));
+router.use(flash())
+router.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
 router.use(passport.initialize())
 router.use(passport.session())
 
@@ -33,6 +34,43 @@ router.use(passport.session())
 //         res.redirect('/')
 //       })
 // })
+
+
+//=================================================
+//          ROUTES FOR LOGIN/REGISTER
+//=================================================
+
+//index
+router.get('/login' , (req, res) => { 
+    res.render('home.ejs')
+})
+
+router.get('/register' , (req, res) => { 
+    res.render('register.ejs')
+})
+
+router.post('/login', passport.authenticate('local', 
+{
+    successRedirect:'/',
+    failureRedirect: '/login',
+    failureFlash: true,
+}
+)) 
+
+router.post('/register' , async (req, res) => { 
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        User.create([{
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+        }], (error, User) => {
+            res.redirect('/login')
+        }) 
+    } catch (error) {
+        res.redirect('/register')
+    }
+})
 
 //=================================================
 //              CHECKING AUTHENTICATION
@@ -51,43 +89,6 @@ const checkNotAuthenticated = (req, res, next) => {
         return next()
     }
 }
-//=================================================
-//          ROUTES FOR LOGIN/REGISTER
-//=================================================
-
-//index
-router.get('/login', checkNotAuthenticated, (req, res) => { 
-    res.render('home.ejs')
-})
-
-router.get('/register', checkNotAuthenticated, (req, res) => { 
-    res.render('register.ejs')
-})
-
-router.post('/', checkNotAuthenticated, passport.authenticate('local', 
-{
-    successRedirect:'/',
-    failureRedirect: '/login',
-    failureFlash: true,
-}
-)) 
-
-
-router.post('/register', checkNotAuthenticated, async (req, res) => { 
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        User.create([{
-            id: Date.now().toString,
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword,
-        }], (error, User) => {
-            res.redirect('/login')
-        }) 
-    } catch (error) {
-        res.redirect('/register')
-    }
-})
 
 //=================================================
 //          ROUTES AFTER AUTHENTICATION
@@ -98,18 +99,14 @@ router.get('/new', (req, res) => {
     res.render('new.ejs')
 })
 
-router.get('/', checkAuthenticated, (req, res) => {
-    List.find({}, (error, showList) => {
-        res.render('show.ejs', 
-        {
-            list: showList,
-            name: req.user.name
-        })
+router.get('/', (req, res) => {
+    List.create({}, (error, List) => {
+        res.render('index.ejs')
     })
 })
 
 //show
-router.get('/:id/', checkAuthenticated , (req, res) => {
+router.get('/:id/', (req, res) => {
     List.findById(req.params.id, (error, showList) => {
         res.render('show.ejs', 
         {
@@ -119,7 +116,7 @@ router.get('/:id/', checkAuthenticated , (req, res) => {
 })
 
 //new post
-router.post('/', checkAuthenticated, (req, res) => {
+router.post('/', (req, res) => {
     List.create(req.body, (error, List) => {
         res.redirect('/')
     })
@@ -127,7 +124,7 @@ router.post('/', checkAuthenticated, (req, res) => {
 
 
 //edit
-router.get('/:id/edit', checkAuthenticated, (req, res) => {
+router.get('/:id/edit', (req, res) => {
     List.findById(req.params.id, (error, editList) => {
         res.render('edit.ejs', 
         {
@@ -137,19 +134,175 @@ router.get('/:id/edit', checkAuthenticated, (req, res) => {
 })
 
 //update
-router.put('/:id/', checkAuthenticated, (req, res) => {
+router.put('/:id/', (req, res) => {
     List.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedList) => {
       res.render('show.ejs', {list: updatedList})
     })
 });
 
 //delete
-router.delete('/:id', checkAuthenticated, (req, res) => {
+router.delete('/:id', (req, res) => {
     List.findByIdAndRemove(req.params.id, (error, data) => {
         res.redirect('/')
     })
 })
 
-
-
 module.exports = router
+
+// if(process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config()
+// }
+// const express = require('express')
+// const bcrypt = require('bcrypt')
+// const router = express.Router()
+// const User = require('../models/users.js')
+// const List = require('../models/list.js')
+
+// const passport = require('passport')
+// const flash = require('express-flash')
+// const session = require('express-session')
+
+// const initializePassport = require('../passport-config.js')
+// initializePassport(
+//     passport, 
+//     email => users.find(user => user.email === email),
+//     id => users.find(user => user.id === id)
+// )
+
+// router.use(flash)
+// router.use(
+//     session({
+//       secret: process.env.SECRET, 
+//       resave: false, 
+//       saveUninitialized: false 
+//     })
+//   )
+// router.use(passport.initialize())
+// router.use(passport.session())
+
+// //seed
+// // router.get('/seed', (req,res) => {
+// //     List.create(seed, (error, data) => {
+// //         res.redirect('/')
+// //       })
+// // })
+
+// //=================================================
+// //              CHECKING AUTHENTICATION
+// //=================================================
+// const checkAuthenticated = (req, res, next) => {
+//     if (req.isAuthenticated()) {
+//         return next()
+//     } else {
+//     res.redirect('/login')
+//     }
+// }
+// const checkNotAuthenticated = (req, res, next) => {
+//     if (req.isAuthenticated()) {
+//         res.redirect('/')
+//     } else {
+//         return next()
+//     }
+// }
+// //=================================================
+// //          ROUTES FOR LOGIN/REGISTER
+// //=================================================
+
+// //index
+// router.get('/login', checkNotAuthenticated, (req, res) => { 
+//     res.render('home.ejs')
+// })
+
+// router.get('/register', checkNotAuthenticated, (req, res) => { 
+//     res.render('register.ejs')
+// })
+
+// router.post('/login', checkNotAuthenticated, passport.authenticate('local', 
+// {
+//     successRedirect:'/',
+//     failureRedirect: '/login',
+//     failureFlash: true,
+// }
+// )) 
+
+
+// router.post('/register', checkNotAuthenticated, async (req, res) => { 
+//     try {
+//         const hashedPassword = await bcrypt.hash(req.body.password, 10)
+//         User.create([{
+//             id: Date.now().toString,
+//             name: req.body.name,
+//             email: req.body.email,
+//             password: hashedPassword,
+//         }], (error, User) => {
+//             res.redirect('/login')
+//         }) 
+//     } catch (error) {
+//         res.redirect('/register')
+//     }
+// })
+
+// //=================================================
+// //          ROUTES AFTER AUTHENTICATION
+// //=================================================
+
+// //new
+// router.get('/new', (req, res) => {
+//     res.render('new.ejs')
+// })
+
+// router.get('/', checkAuthenticated, (req, res) => {
+//     List.find({}, (error, showList) => {
+//         res.render('show.ejs', 
+//         {
+//             list: showList,
+//             name: req.user.name
+//         })
+//     })
+// })
+
+// //show
+// router.get('/:id/', checkAuthenticated , (req, res) => {
+//     List.findById(req.params.id, (error, showList) => {
+//         res.render('show.ejs', 
+//         {
+//             list: showList
+//         })
+//     })
+// })
+
+// //new post
+// router.post('/', checkAuthenticated, (req, res) => {
+//     List.create(req.body, (error, List) => {
+//         res.redirect('/')
+//     })
+// })
+
+
+// //edit
+// router.get('/:id/edit', checkAuthenticated, (req, res) => {
+//     List.findById(req.params.id, (error, editList) => {
+//         res.render('edit.ejs', 
+//         {
+//             list: editList
+//         })
+//     })
+// })
+
+// //update
+// router.put('/:id/', checkAuthenticated, (req, res) => {
+//     List.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedList) => {
+//       res.render('show.ejs', {list: updatedList})
+//     })
+// });
+
+// //delete
+// router.delete('/:id', checkAuthenticated, (req, res) => {
+//     List.findByIdAndRemove(req.params.id, (error, data) => {
+//         res.redirect('/')
+//     })
+// })
+
+
+
+// module.exports = router
--- server.js
+++ server.js
@@ -9,34 +9,36 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
-const bcrypt = require('bcrypt')
-const passport = require('passport')
-const flash = require('express-flash')
 const session = require('express-session')
-
-const initializePassport = require('./passport-config.js')
-const listController = require('./controllers/default.js')
-const User = require('./models/users.js')
-
-
+const bcrypt = require('bcrypt')
 const app = express()
-initializePassport(
-    passport, 
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-)
+
+
 
 //=================================================
 //                   MIDDLEWARE
 //=================================================
-app.set('view-engine', 'ejs')
+
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 
 
+const listController = require('./controllers/default.js')
 app.use(listController)
 
+
+// app.use(
+//     session({
+//       secret: process.env.SECRET, 
+//       resave: false, 
+//       saveUninitialized: false 
+//     })
+//   )
+  
+//=================================================
+//                      ROUTES
+//=================================================
 
 
 
@@ -54,3 +56,63 @@ app.listen(process.env.PORT || 3000, ()=>{
 mongoose.connect('mongodb+srv://samhan104:Rl0gW1tvQNsSzfsV@cluster0.gr8z5kk.mongodb.net/?retryWrites=true&w=majority', () => {
   console.log('The connection with mongod is established')
 })
+
+
+
+
+// //=================================================
+// //                   DEPENDENCIES
+// //=================================================
+
+// if(process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config()
+// }
+
+// const express = require('express')
+// const mongoose = require('mongoose')
+// const methodOverride = require('method-override')
+// const bcrypt = require('bcrypt')
+// const passport = require('passport')
+// const flash = require('express-flash')
+// const session = require('express-session')
+
+// const initializePassport = require('./passport-config.js')
+// const listController = require('./controllers/default.js')
+// const User = require('./models/users.js')
+
+
+// const app = express()
+// initializePassport(
+//     passport, 
+//     email => users.find(user => user.email === email),
+//     id => users.find(user => user.id === id)
+// )
+
+// //=================================================
+// //                   MIDDLEWARE
+// //=================================================
+// app.set('view-engine', 'ejs')
+// app.use(express.static('public'));
+// app.use(express.urlencoded({extended: false}));
+// app.use(methodOverride('_method'));
+
+
+// app.use(listController)
+
+
+
+
+// //=================================================
+// //                   CONNECTION
+// //=================================================
+// app.listen(process.env.PORT || 3000, ()=>{
+// 	console.log('listening'); 
+// })
+
+// // mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+// // const db = mongoose.connection
+// // db.on('error', error => console.error(error))
+// // db.once('open', () => console.log("Connected to mongo"))
+// mongoose.connect('mongodb+srv://samhan104:Rl0gW1tvQNsSzfsV@cluster0.gr8z5kk.mongodb.net/?retryWrites=true&w=majority', () => {
+//   console.log('The connection with mongod is established')
+// })
