0ff6e653c7466bf003a98074625f43e58ab38401
General Assembly <generalassembly@tests-MacBook-Air.local>
2022-11-30 01:56:08 -0500

    I have to relearn everything I did for my start

--- server.js
+++ server.js
@@ -27,62 +27,62 @@ const listController = require('./controllers/default.js')
 app.use(listController)
 
 //seed
-// app.get('/seed', (req,res) => {
-//     List.create(seed, (error, data) => {
-//         res.redirect('/')
-//       })
-// })
+app.get('/seed', (req,res) => {
+    List.create(seed, (error, data) => {
+        res.redirect('/')
+      })
+})
 
 
-// //index
-// app.get('/' , (req, res) => { //list will be shown on index. in show, list will show items within
-//     List.find({}, (error, getList) => {
-//         res.render('index.ejs', 
-//         {
-//             list: getList
-//         })
-//     })
-// })
+//index
+app.get('/' , (req, res) => { //list will be shown on index. in show, list will show items within
+    List.find({}, (error, getList) => {
+        res.render('index.ejs', 
+        {
+            list: getList
+        })
+    })
+})
 
-// //new
-// app.get('/new', (req, res) => {
-//     res.render('new.ejs')
-// })
+//new
+app.get('/new', (req, res) => {
+    res.render('new.ejs')
+})
 
-// //show
-// app.get('/:id/', (req, res) => {
-//     List.findById(req.params.id, (error, showList) => {
-//         res.render('show.ejs', 
-//         {
-//             list: showList
-//         })
-//     })
-// })
+//show
+app.get('/:id/', (req, res) => {
+    List.findById(req.params.id, (error, showList) => {
+        res.render('show.ejs', 
+        {
+            list: showList
+        })
+    })
+})
 
-// //new post
-// app.post('/', (req, res) => {
-//     List.create(req.body, (error, List) => {
-//         res.redirect('/')
-//     })
-// })
+//new post
+app.post('/', (req, res) => {
+    List.create(req.body, (error, List) => {
+        res.redirect('/')
+    })
+})
 
 
-// //edit
-// app.get('/:id/edit', (req, res) => {
-//     List.findById(req.params.id, (error, editList) => {
-//         res.render('edit.ejs', 
-//         {
-//             list: editList
-//         })
-//     })
-// })
+//edit
+app.get('/:id/edit', (req, res) => {
+    List.findById(req.params.id, (error, editList) => {
+        res.render('edit.ejs', 
+        {
+            list: editList
+        })
+    })
+})
 
-// //update
-// app.put('/:id/', (req, res) => {
-//     List.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedList) => {
-//       res.render('show.ejs', {list: updatedList})
-//     })
-// });
+//update
+app.put('/:id/', (req, res) => {
+    List.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedList) => {
+      res.render('show.ejs', {list: updatedList})
+    })
+});
 
 //=================================================
 //                   CONNECTION
